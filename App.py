a57194e834db2e2c498d3be449154d0821179807
Neeraj Pokala <73011282+Neerajpokala@users.noreply.github.com>
2024-04-18 20:26:44 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,34 +1,28 @@
 import streamlit as st
 from googletrans import Translator, LANGUAGES
 
-def translate_text(input_text):
+def translate_text(input_text, target_lang_key):
     try:
         translator = Translator()
-
-        if not input_text:
-            st.warning("Please enter text to translate.")
-            return
-        
-        detection = translator.detect(input_text)
-        source_lang = detection.lang
-        
-        if source_lang:
-            target_lang = st.selectbox("Select the target language:", [LANGUAGES[lang] for lang in LANGUAGES])
-            target_lang_key = list(LANGUAGES.keys())[list(LANGUAGES.values()).index(target_lang)]
-
-            if st.button("Translate"):
-                translated_text = translator.translate(input_text, src=source_lang, dest=target_lang_key)
-                st.success(f"Translated text ({target_lang}): {translated_text.text}")
-        else:
-            st.warning("Language detection failed. Please provide text in a known language.")
+        translated_text = translator.translate(input_text, dest=target_lang_key)
+        return translated_text.text
     except Exception as e:
         st.error(f"An error occurred: {e}")
 
 def main():
     st.set_page_config(page_title="Text Translator", page_icon="🌎")
     st.title("Text Translator Supports 107 languages")
+
+    # Always show the selectbox and translate button
+    target_lang = st.selectbox("Select the target language:", [LANGUAGES[lang] for lang in LANGUAGES])
+    target_lang_key = list(LANGUAGES.keys())[list(LANGUAGES.values()).index(target_lang)]
+    translate_button = st.button("Translate")
+
     input_text = st.text_area("Enter the text to be translated:")
-    translate_text(input_text)
+
+    if translate_button and input_text:
+        translated_text = translate_text(input_text, target_lang_key)
+        st.success(f"Translated text ({target_lang}): {translated_text}")
 
 if __name__ == "__main__":
     main()
