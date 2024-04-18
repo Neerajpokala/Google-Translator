587f10d13d9ea0d34183b407358aa66276638a2f
Neeraj Pokala <73011282+Neerajpokala@users.noreply.github.com>
2024-04-18 20:19:29 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,24 +1,34 @@
 import streamlit as st
 from googletrans import Translator, LANGUAGES
 
-def translate_text(input_text):
-    translator = Translator()
-    detection = translator.detect(input_text)
-    source_lang = detection.lang if input_text else None
+def translate_text():
+    try:
+        translator = Translator()
+        input_text = st.text_area("Enter the text to be translated:")
+        
+        if not input_text:
+            st.warning("Please enter text to translate.")
+            return
+        
+        detection = translator.detect(input_text)
+        source_lang = detection.lang
+        
+        if source_lang:
+            target_lang = st.selectbox("Select the target language:", [LANGUAGES[lang] for lang in LANGUAGES])
+            target_lang_key = list(LANGUAGES.keys())[list(LANGUAGES.values()).index(target_lang)]
 
-    if source_lang:
-        target_lang = st.selectbox("Select the target language:", [LANGUAGES[lang] for lang in LANGUAGES])
-        target_lang_key = list(LANGUAGES.keys())[list(LANGUAGES.values()).index(target_lang)]
-
-        if st.button("Translate"):
-            translated_text = translator.translate(input_text, src=source_lang, dest=target_lang_key)
-            st.success(f"Translated text ({target_lang}): {translated_text.text}")
+            if st.button("Translate"):
+                translated_text = translator.translate(input_text, src=source_lang, dest=target_lang_key)
+                st.success(f"Translated text ({target_lang}): {translated_text.text}")
+        else:
+            st.warning("Language detection failed. Please provide text in a known language.")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
 
 def main():
     st.set_page_config(page_title="Text Translator", page_icon="🌎")
     st.title("Text Translator Supports 107 languages")
-    input_text = st.text_area("Enter the text to be translated:")
-    translate_text(input_text)
+    translate_text()
 
 if __name__ == "__main__":
     main()
